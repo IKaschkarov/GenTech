53898706c0c339f4afb97bae8f67c1fb1620044a
IKaschkarov <142896713+IKaschkarov@users.noreply.github.com>
2023-12-09 19:38:47 +0100

    add HW_30_11

---
+++ HW30_11.sql
@@ -0,0 +1,38 @@
+Задача 1. Вывести название и стоимость в USD одного самого дорогого проданного товара
+
+
+SELECT 
+Products.ProductName,
+Products.Price AS Price_EUR,
+Products.Price * 1.08 AS Price_USD
+FROM Products
+
+ORDER BY Price DESC
+LIMIT 1;
+
+
+Задача 2. Вывести два самых дорогих товара из категории Beverages из USA
+
+
+SELECT * 
+FROM Categories
+
+JOIN Products ON Categories.CategoryID = Products.CategoryID
+JOIN Suppliers ON Suppliers.SupplierID = Products.SupplierID
+
+WHERE Suppliers.Country = 'USA' AND  Categories.CategoryName = 'Beverages'
+
+ORDER BY Price DESC
+LIMIT 2
+
+
+Задача 3. Вывести список стран, которые поставляют морепродукты
+
+SELECT 
+Suppliers.Country
+FROM Products
+
+JOIN Categories ON Categories.CategoryID = Products.CategoryID
+JOIN Suppliers ON Suppliers.SupplierID = Products.SupplierID
+
+WHERE Categories.CategoryName = 'Seafood'
